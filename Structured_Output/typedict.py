c04c5fef3885acf7d116b5c02f63d9aab6453c30
trivediadi <trivediadi667@gmail.com>
2025-08-06 23:43:26 +0530

    Refactor review TypedDict to include annotations for summary and sentiment

--- Structured_Output/typedict.py
+++ Structured_Output/typedict.py
@@ -1,14 +1,14 @@
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
-from typing import TypedDict
+from typing import TypedDict,Annotated
 
 load_dotenv()
 
 model=ChatOpenAI(model="gpt-3.5-turbo", temperature=0.0)
 
 class review(TypedDict):
-    summary: str
-    sentiment:str
+    summary: Annotated[str, "Summary of the review"]
+    sentiment:Annotated[str, "Sentiment of the review (positive, negative, neutral)"]
 
 structured_model=model.with_structured_output(review)
 result = structured_model.invoke(""" The hardware is great,but the software feels bloated. There are too many pre-installed apps that I never use. I wish I could remove them easily. Hoping for a software update that allows more customization.""")
