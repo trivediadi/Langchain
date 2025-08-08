3c96d098a5ffdc3a9316ca1f059a6526d09a1bad
trivediadi <trivediadi667@gmail.com>
2025-08-08 20:16:35 +0530

    Add PydanticOutputParser implementation for character data extraction

---
+++ OutPut_Parser/PydanticOutputParser.py
@@ -0,0 +1,31 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
+from pydantic import BaseModel, Field
+
+load_dotenv()
+
+llm =HuggingFaceEndpoint(
+    repo_id="mistralai/Mixtral-8x7B-Instruct-v0.1",
+    task="text-generation"
+)
+model=ChatHuggingFace(llm=llm)
+ 
+class Person(BaseModel):
+    name: str = Field(description="The name of the person")
+    age: int = Field(description="The age of the person")
+    city: str = Field(description="The city where the person lives")
+
+parser = PydanticOutputParser(pydantic_object=Person)
+
+template = PromptTemplate(
+    template="Give me the name, age and city of a cartoon character Ben10. \n {format_instruction}",
+    input_variables=[],
+    partial_variables={"format_instruction": parser.get_format_instructions()}
+)
+prompt=template.format()
+result=model.invoke(prompt)
+final_result = parser.parse(result.content)
+
+print(final_result)
